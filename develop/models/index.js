65473a4fe8836b646ee95d20acfa88fba4ef4ed2
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-11-15 09:23:44 -0800

    added to index.js in models folder

--- develop/models/index.js
+++ develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Product.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: "ProductTag",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Product, {
+  through: "ProductTag",
+});
 
 module.exports = {
   Product,
